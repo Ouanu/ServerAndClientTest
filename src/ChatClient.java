d43b4d78d9888e39e6aa055c290aaf925002ac8d
Ouanu <ouanu@foxmail.com>
2021-09-18 16:36:07 +0800

    ChatServer

---
+++ src/ChatClient.java
@@ -0,0 +1,11 @@
+public class ChatClient {
+    private String host = "localhost";
+    private int port = 7290;
+
+    public ChatClient() {}
+
+    public ChatClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+}
---
+++ src/ChatServer.java
@@ -0,0 +1,48 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ChatServer {
+    private int port = 7290;
+
+    public ChatServer() {
+    }
+
+    public ChatServer(int port) {
+        this.port = port;
+    }
+
+    public void server() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            Socket socket = serverSocket.accept();
+
+            try {
+                DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+                DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+                Scanner scanner = new Scanner(System.in);
+
+                while (true) {
+                    String accpet = inputStream.readUTF();
+                    System.out.println(accpet);
+                    String send = scanner.nextLine();
+                    System.out.println("服务器：" + send);
+                    outputStream.writeUTF("服务器：" + send);
+                }
+            } finally {
+                socket.close();
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        new ChatServer().server();
+    }
+}
